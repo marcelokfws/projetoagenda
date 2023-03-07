6a5c130ca1bc639962ae7f190e34ddacab8f9b6e
marcelokfws <marcelokfws>
2023-03-07 01:06:36 -0300

    .

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const MongoStore = require('connect-mongo');
 const flash = require('connect-flash');
 const routes = require('./routes');
 const path = require('path');
-const helmet = require('helmet');
+// const helmet = require('helmet');
 const csrf = require('csurf');
 const { middlewareGlobal, checkCsrfError, csrfMiddleware } = require('./src/middlewares/middleware');
 
